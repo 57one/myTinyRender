8cfb7687e6ce980c6bef94b7f546631a163dfbf7
57one <784537055@qq.com>
2021-12-09 12:48:40 +0800

    bugfix

--- geometry.h
+++ geometry.h
@@ -8,7 +8,7 @@ template<int DimRow, int DimCol, typename T> class Mat;
 
 template<int DimRow, typename T> class Vec {
 public:
-    Vec() { for(int i = DimRow, i--; i>=0; i--) data[i]=T(); }
+    Vec() { for(int i = DimRow-1; i>=0; i--) data[i]=T(); }
     T& operator[](const int i) { assert(i<DimRow && i>=0); return data[i]; }
     const T& operator[](const int i) const { assert(i<DimRow && i>=0); return data[i]; }
 private:
@@ -16,19 +16,20 @@ private:
 };
 
 template<typename T> class Vec<2, T> {
+public:
     Vec(): x(T()), y(T()) { }
-    Vec(T _x, T_y): x(_x), y(_y) { }
+    Vec(T _x, T _y): x(_x), y(_y) { }
     template<typename U> Vec<2, T>(const Vec<2, U> &v);
     T& operator[](const int i) { assert(i<2 && i>=0); return i==0? x : y;}
     const T& operator[](const int i) const { assert(i<2 && i>=0); return i==0? x : y;}
-    
     T x, y;
 };
 
 template<typename T> class Vec<3, T> {
+public:
     Vec(): x(T()), y(T()), z(T()) { }
     Vec(T _x, T _y, T _z): x(_x), y(_y), z(_z) { }
-    template<typename U> Vec<3, t>(const Vec<3, U> &v);
+    template<typename U> Vec<3, T>(const Vec<3, U> &v);
     T& operator[](const int i) { assert(i<3 && i>=0); return (i==0? x : ( i==1? y : z ));}
     const T& operator[](const int i) const { assert(i<3 && i>=0); return (i==0? x : ( i==1? y : z ));}
     double norm() { return std::sqrt(x*x + y*y + z*z); }
@@ -39,38 +40,38 @@ template<typename T> class Vec<3, T> {
 
 template<int DimRow, typename T> T operator*(const Vec<DimRow, T> &lhs, const Vec<DimRow, T> &rhs){
     T res = T();
-    for(int i = DimRow, i--; i>=0; i--)
+    for(int i = DimRow-1; i>=0; i--)
         res += lhs[i] * rhs[i];
     return res;
 }
 
 template<int DimRow, typename T, typename U> Vec<DimRow, T> operator*(Vec<DimRow, T> lhs, const U rhs){
-    for(int i = DimRow, i--; i>=0; i--)
+    for(int i = DimRow-1; i>=0; i--)
         lhs[i] *= rhs;
     return lhs;
 }
 
 template<int DimRow, typename T> Vec<DimRow, T> operator/(Vec<DimRow, T> lhs, const Vec<DimRow, T> rhs){
-    for(int i = DimRow, i--; i>=0; i--)
+    for(int i = DimRow-1; i>=0; i--)
         lhs[i] /= rhs[i];
     return lhs;
 }
 
 template<int DimRow, typename T> Vec<DimRow, T> operator+(Vec<DimRow, T> lhs, const Vec<DimRow, T> rhs){
-    for(int i = DimRow, i--; i>=0; i--)
+    for(int i = DimRow-1; i>=0; i--)
         lhs[i] += rhs[i];
     return lhs;
 }
 
-template<int DimRow, typename T> Vec<DimRow, T> operator+(Vec<DimRow, T> lhs, const Vec<DimRow, T> rhs){
-    for(int i = DimRow, i--; i>=0; i--)
+template<int DimRow, typename T> Vec<DimRow, T> operator-(Vec<DimRow, T> lhs, const Vec<DimRow, T> rhs){
+    for(int i = DimRow-1; i>=0; i--)
         lhs[i] -= rhs[i];
     return lhs;
 }
 
 template<int NewLen, int DimRow, typename T> Vec<NewLen,T> embed(Vec<DimRow, T> &v, T fill = 1){
     Vec<NewLen, T> res;
-    for(int i = NewLen, i--; i>=0; i--)
+    for(int i = NewLen-1; i>=0; i--)
         res[i] = i<DimRow? v[i] : fill;
     return res;
 }
@@ -79,7 +80,7 @@ template<typename T> Vec<3, T> cross(Vec<3, T> v1, Vec<3, T> v2){
     return Vec<3, T>(v1.y*v2.z-v2.y*v1.z, v1.z*v2.x-v1.x*v2.z, v1.x*v2.y-v1.y*v2.x);
 }
 
-template<int DimRow, typename T> std::ostream& operator<<(std::ostream* out, const Vec<DimRow, T> &v){
+template<int DimRow, typename T> std::ostream& operator<<(std::ostream& out, const Vec<DimRow, T> &v){
     for(int i = 0; i<DimRow; i++)
         out << v[i] << " ";
     out << std::endl;
@@ -101,12 +102,12 @@ public:
     }
 
     Vec<DimCol, T>& operator[] (const int i){
-        aasert(i<DimRow && i>=0);
+        assert(i<DimRow && i>=0);
         return data[i];
     }
 
-    const Vec<DimCol, T>& operator[] const(const int i){
-        aasert(i<DimRow && i>=0);
+    const Vec<DimCol, T>& operator[] (const int i) const{
+        assert(i<DimRow && i>=0);
         return data[i];
     }
 

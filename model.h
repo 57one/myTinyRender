a740bc7b2e878bcf857fbb366f7ea37b5d7ddc4e
57one <784537055@qq.com>
2021-12-09 16:57:40 +0800

    add texture property and method

--- model.h
+++ model.h
@@ -7,13 +7,16 @@
 class Model {
 private:
     std::vector<Vec3f> verts_;
+    std::vector<Vec3f> uv_;
     std::vector<std::vector<Vec3i>> faces_; //vertex vertex_texture vertex_normal
+    
 public:
     Model(const char *filename);
     ~Model();
     int nverts();
     int nfaces();
     Vec3f vert(int i);
+    Vec3f uv(int iface, int nthvert);
     std::vector<int> face(int i);
 };
 

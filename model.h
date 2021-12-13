48d5bcc88e8e10961e5a2adacef61782e1692b27
57one <784537055@qq.com>
2021-12-13 23:59:08 +0800

    add norms_ and normal()

--- model.h
+++ model.h
@@ -8,6 +8,7 @@ class Model {
 private:
     std::vector<Vec3f> verts_;
     std::vector<Vec3f> uv_;
+    std::vector<Vec3f> norms_;
     std::vector<std::vector<Vec3i>> faces_; //vertex vertex_texture vertex_normal
     
 public:
@@ -16,7 +17,9 @@ public:
     int nverts();
     int nfaces();
     Vec3f vert(int i);
+    Vec3f vert(int iface, int nthvert);
     Vec3f uv(int iface, int nthvert);
+    Vec3f normal(int iface, int nthvert);
     std::vector<int> face(int i);
 };
 
